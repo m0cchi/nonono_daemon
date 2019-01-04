f48630fdf30b519fd1041dd3f119cc85f650fec4
mocchi <boom.boom.planet@gmail.com>
2019-01-04 22:42:08 +0900

    initial commit

---
+++ lib/nonono_daemon.rb
@@ -0,0 +1,6 @@
+require "nonono_daemon/version"
+
+module NononoDaemon
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/nonono_daemon/version.rb
@@ -0,0 +1,3 @@
+module NononoDaemon
+  VERSION = "0.1.0"
+end
